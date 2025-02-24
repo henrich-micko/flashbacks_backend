505a2007912557737c2209d4490cbdadccac3e25
Henri <henrich.micko@spsjm.eu>
2025-02-24 13:03:06 +0100

    cong asgi

--- backend/asgi.py
+++ backend/asgi.py
@@ -1,14 +1,15 @@
 import os
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chat.routing import websocket_urlpatterns
 from chat.middleware import TokenAuthMiddleware
 
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": TokenAuthMiddleware(
         URLRouter(websocket_urlpatterns)
     ),

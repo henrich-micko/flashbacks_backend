55326e1df4ef2586de25fa6d575bcbaa6caee4c5
Henri <henrich.micko@spsjm.eu>
2025-02-24 13:08:16 +0100

    load dotenv

--- backend/settings.py
+++ backend/settings.py
@@ -15,7 +15,9 @@ import os
 from celery import Celery
 from celery.schedules import crontab
 from django.core.management.utils import get_random_secret_key
+from dotenv import load_dotenv
 
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
